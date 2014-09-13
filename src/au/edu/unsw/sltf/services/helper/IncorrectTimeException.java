65a7667c0a69a3354cd028ffee6354fe5def317c
hariAtThinkBright <hari@thinkbrightmedia.com.au>
2014-09-13 15:01:53 +1000

    More faults

---
+++ src/au/edu/unsw/sltf/services/helper/IncorrectTimeException.java
@@ -0,0 +1,12 @@
+package au.edu.unsw.sltf.services.helper;
+
+public class IncorrectTimeException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public IncorrectTimeException() {
+		super();
+	}
+}
